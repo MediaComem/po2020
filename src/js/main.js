6ff21822b69cebd65c26ddaf9e6d07dc278086cc
Imagoiq <git.noreply@posteo.no>
2020-03-24 12:58:17 +0100

    4 first slid beta ready for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-import 'alpinejs';
+// import 'alpinejs';
+import Glide, { Controls, Autoplay , Swipe } from '@glidejs/glide/dist/glide.modular.esm';
+
+new Glide('.glide',{
+    //autoplay: 4000,
+    hoverpause:true
+}).mount({ Controls, Autoplay, Swipe })
