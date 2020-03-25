d422206b4452eff9cfcf960795da1a981deeed75
Imagoiq <git.noreply@posteo.no>
2020-03-25 11:01:04 +0100

    Change duration of slider animation to 6s

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 import Glide, { Controls, Autoplay , Swipe } from '@glidejs/glide/dist/glide.modular.esm';
 
 new Glide('.glide',{
-    autoplay: 4000,
+    autoplay: 6000,
     hoverpause:true
 }).mount({ Controls, Autoplay, Swipe })
 
