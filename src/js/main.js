1371aa346f58dc0172fb22f7a6474052bcdcc9dd
Imagoiq <git.noreply@posteo.no>
2020-03-25 11:26:42 +0100

    Add 1s duration to autoplay

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 import Glide, { Controls, Autoplay , Swipe } from '@glidejs/glide/dist/glide.modular.esm';
 
 new Glide('.glide',{
-    autoplay: 8000,
+    autoplay: 9000,
     hoverpause:true
 }).mount({ Controls, Autoplay, Swipe })
 
