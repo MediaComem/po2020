04f125eef1f9551960e1176656e6ab24db6ca2a7
Imagoiq <git.noreply@posteo.no>
2020-03-25 11:02:09 +0100

    Slider Autoplay: 8s

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 import Glide, { Controls, Autoplay , Swipe } from '@glidejs/glide/dist/glide.modular.esm';
 
 new Glide('.glide',{
-    autoplay: 6000,
+    autoplay: 8000,
     hoverpause:true
 }).mount({ Controls, Autoplay, Swipe })
 
