137e9cd281ec4a6d63894215253ffa2f09767d42
Imagoiq <git.noreply@posteo.no>
2020-03-31 20:10:06 +0200

    FAQ

--- src/js/main.js
+++ src/js/main.js
@@ -23,4 +23,59 @@ deviensim.mount({ Controls, Autoplay, Swipe });
 
 
 
-screen.lockOrientation('portrait');
+screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
+
+if (screen.lockOrientationUniversal("portrait")) {
+    console.log("Orientation was locked");
+} else {
+    console.log("Orientation lock failed");
+}
+
+
+/* Form from Formspree */
+
+window.addEventListener("DOMContentLoaded", function () {
+
+    // get the form elements defined in your form HTML above
+
+    var form = document.getElementById("newquestion");
+    var button = document.getElementById("newquestion-button");
+    var status = document.getElementById("newquestion-status");
+
+    // Success and Error functions for after the form is submitted
+
+    function success() {
+        form.reset();
+        button.style = "display: none ";
+        status.innerHTML = "Merci pour ta question!";
+    }
+
+    function error() {
+        status.innerHTML = "Oups! Il y a eu un problème, réessaye plus tard.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
