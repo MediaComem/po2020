47d59393d1e3ede724c18161a3ac9ad24e92f214
Imagoiq <git.noreply@posteo.no>
2020-03-31 18:43:34 +0200

    Boussole fixes + desktop

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import 'alpinejs';
-import Glide, { Controls, Autoplay , Swipe } from '@glidejs/glide/dist/glide.modular.esm';
+import Glide, { Breakpoints, Controls, Autoplay , Swipe } from '@glidejs/glide/dist/glide.modular.esm';
 
 var deviensim = new Glide('.deviensim_glide',{
     autoplay: 9000,
@@ -7,10 +7,18 @@ var deviensim = new Glide('.deviensim_glide',{
 });
 
 var boussole = new Glide('.boussole_glide',{
-    gap: 0
+    gap: 0,
+    breakpoints:{
+        767: {
+            perView: 1
+        },
+        5000: {
+            perView: 3
+        }
+    }
 });
 
-boussole.mount({ Controls, Swipe });
+boussole.mount({ Breakpoints, Controls, Swipe });
 deviensim.mount({ Controls, Autoplay, Swipe });
 
 
