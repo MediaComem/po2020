05b6fb955047d4851928c9a0a4e958c41fa15bf6
Imagoiq <git.noreply@posteo.no>
2020-03-31 17:37:42 +0200

    Boussole

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
-// import 'alpinejs';
+import 'alpinejs';
 import Glide, { Controls, Autoplay , Swipe } from '@glidejs/glide/dist/glide.modular.esm';
 
-new Glide('.glide',{
+var deviensim = new Glide('.deviensim_glide',{
     autoplay: 9000,
     hoverpause:true
-}).mount({ Controls, Autoplay, Swipe })
+});
+
+var boussole = new Glide('.boussole_glide',{
+    gap: 0
+});
+
+boussole.mount({ Controls, Swipe });
+deviensim.mount({ Controls, Autoplay, Swipe });
+
+
 
 screen.lockOrientation('portrait');
